152cc0f774195e69917f5a77e35e327e4bb25ae3
Nick White <nwhite@chipublib.org>
2017-09-19 11:57:52 -0500

    adds various php parts

---
+++ php/MillenniumPark.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Millennium Park | Digital Collections | Chicago Public Library</title>
+	    <link rel="shortcut icon" type="image/x-icon" href="https://chicago.bibliocms.com/wp-content/uploads/sites/3/2016/04/CPLOpenBook_web_16x16-1.png">
+		<link rel="apple-touch-icon-precomposed" type="image/x-icon" href="https://chicago.bibliocms.com/wp-content/uploads/sites/3/2016/04/CPLreverse_web_200x200-150x150.png">
+		<link rel="stylesheet" href="cpldc.css">
+		<script type="text/javascript" src="cpldc.js" ></script>
+</head>
+<body>
+<?php 
+	include 'header.html';
+?>
+<div class="body-full-f">
+<?php
+	include 'sidebar-nav-fixed.html';
+?>
+	<div class="main-content-f">
+		<input type="checkbox" id="toggle-mrc" checked>
+			<label for="toggle-mrc">
+				<h2 class="main-content-f-title">
+					Millennium Park Construction Collection
+				</h2>
+			</label>
+		</input>
+		<article id="expand-mrc" class="mrc-copy">
+			<span>Watch the site transform from an industrial wasteland to a new urban park of world renown.</span>
+			<div class="location-card">
+				<img src="http://digital.chipublib.org/digital/api/singleitem/image/mpu/6324/default.jpg" />
+				<figcaption>
+					Full Park, construction, April 20, 2004
+				</figcaption>
+			</div>
+			<span>More Millennium Park copy.</span>
+			<div class="location-card">
+				<img src="http://digital.chipublib.org/digital/api/singleitem/image/mpu/1510/default.jpg" />
+				<figcaption>
+					Cloud Gate, assembly in Chicago, structural steel, ca. March 3, 2004
+				</figcaption>
+			</div>
+			<div class="browse-section">
+				<button class="browse-collection-button" onclick="javascript:location.href='https://chipublib.bibliocommons.com/events/search/index'">
+					Browse the complete collection
+				</button>
+				<div class="browse-filteredlinks">
+					<p>
+						<a href="http://cdm16818.contentdm.oclc.org/cdm/search/collection/mpu/searchterm/highlight/field/highli/mode/all/conn/and/order/origin">
+							Collection highlights
+						</a>
+						<br />
+						<span class="browse-link-caption">
+							Get a taste of how this transformation took place in a selected group of images.
+						</span>
+					</p>
+					<p>
+						<a href="http://chicago.bibliocms.com/fa-millennium-park-u-s-equities-realty-collection/">
+							Millennium Park / U.S. Equities Realty Collection finding aid
+						</a>
+						<br />
+						<span class="browse-link-caption">
+							Learn about the construction of the park and the Chicago-based company that made it possible.
+						</span>
+					</p>
+				</div>
+			</div>
+					<p>
+						<span class="rights">
+							The Millennium Park Digital Collection was made possible by generous gifts from U.S. Equities Realty and the Gaylord and Dorothy Donnelley Foundation through the Chicago Public Library Foundation.
+						</span>
+					</p>
+		</article>
+	</div>
+	<div class="right-sidebar">
+		<div class="blog">
+			<h3>
+				Special Collections Blog
+			</h3>
+			<dl class="blog-list">
+				<dt>Labor of Love: Revs. Addie and Claude Wyatt Photog&hellip; &raquo;</dt>
+				<dd>By: Cynthia August 31, 2017</dd>
+				<dd>Chicagoan Addie Wyatt was a minister, labor activist and&hellip;</dd>
+			</dl>
+			<dl class="blog-list">
+				<dt>Happy 50th to the Chicago Picasso!</dt>
+				<dd>By: Johanna August 4, 2017</dd>
+				<dd>In the early 1960s, the architectural firm Skidmore, Owings &amp; &hellip;</dd>
+			</dl>
+			<dl class="blog-list">
+				<dt>Quiz: How Well Do You Know the Chicago Cubs?</dt>
+				<dd>By: Ellen July 17, 2017</dd>
+				<dd>Can you smell the peanuts and Cracker Jack? It's baseball&hellip;</dd>
+			</dl>
+		</div>
+		<div class="events">
+			<h3>Events</h3>
+			<h4><a href="">Meet the Author: A.C. Barnes <span class="raquo-blue">&raquo;</span></a></h4>
+			<div class="event-location"><a href="">Avalon</a></div>
+			<div class="event-datetime">
+				<div class="event-date">Sep 9</div>
+				<div class="event-time">3:00PM</div>
+			</div>
+			<h4><a href="">Courthouse Discoveries <span class="raquo-blue">&raquo;</span></a></h4>
+			<div class="event-location"><a href="">Sulzer Regional</a></div>
+			<div class="event-datetime">
+				<div class="event-date">Sep 16</div>
+				<div class="event-time">10:00AM</div>
+			</div>
+			<h4><a href="">Latino Punk, History &amp; Music <span class="raquo-blue">&raquo;</span></a></h4>
+			<div class="event-location"><a href="">Lozano</a></div>
+			<div class="event-datetime">
+				<div class="event-date">Sep 16</div>
+				<div class="event-time">1:00PM</div>
+			</div>
+			<h4><a href="">Chicago Transformed: World War I and the Windy City <span class="raquo-blue">&raquo;</span></a></h4>
+			<div class="event-location"><a href="">Clearing</a></div>
+			<div class="event-datetime">
+				<div class="event-date">Sep 18</div>
+				<div class="event-time">6:00PM</div>
+			</div>
+			<h4><a href="">Finding Your Family Tree <span class="raquo-blue">&raquo;</span></a></h4>
+			<div class="event-location"><a href="">Near North</a></div>
+			<div class="event-datetime">
+				<div class="event-date">Sep 18</div>
+				<div class="event-time">6:30PM</div>
+			</div>
+			<span><a href="">View More &raquo;</a></span>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ php/Neighborhoods.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Millennium Park | Digital Collections | Chicago Public Library</title>
+	    <link rel="shortcut icon" type="image/x-icon" href="https://chicago.bibliocms.com/wp-content/uploads/sites/3/2016/04/CPLOpenBook_web_16x16-1.png">
+		<link rel="apple-touch-icon-precomposed" type="image/x-icon" href="https://chicago.bibliocms.com/wp-content/uploads/sites/3/2016/04/CPLreverse_web_200x200-150x150.png">
+		<link rel="stylesheet" href="cpldc.css">
+		<script type="text/javascript" src="cpldc.js" ></script>
+</head>
+<body>
+<?php 
+	include 'header.html';
+?>
+<div class="body-full-f">
+<?php
+	include 'sidebar-nav-fixed.html';
+?>
+	<div class="main-content-f">
+		<div class="main-content-header">
+			<h1>Neighborhoods Collection</h1>
+			<span>Chicago is a city with lots of rich copy for their Neighborhoods Collection.  This paragraph is an excellent example of neighborhoods collection rich copy.  It really grabs the reader and makes them want to read all the rich copy.</span>
+		</div>
+		<article class="article-copy">
+		<h2 class="main-content-title">
+			Ravenswood-Lake View Community Collection
+		</h2>
+		<p>Scenes from daily life in Lake View Township, 1860s-1889, capture the period before Chicago annexed the independent municipality.</p>
+		<div class="article-img">
+			<img src="http://digital.chipublib.org/digital/api/singleitem/image/rvw/4599/default.jpg" />
+			<figcaption>
+				3430 North Lincoln Avenue
+			</figcaption>
+		</div>
+		<span>Photographs from 1889 through the 1990s reflect the evolution of the former township into the Northside communities of Lake View, North Center, Lincoln Square (Ravenswood), Uptown and Edgewater.</span>
+		<div class="article-img">
+			<img src='http://digital.chipublib.org/digital/api/singleitem/image/rvw/4706/default.jpg'  />
+				<!-- <img src="http://digital.chipublib.org/digital/api/singleitem/image/rvw/4706/default.jpg" /> -->
+				<figcaption>
+					Photo of empty lot with a sign stating "Three Links Lodge I.O.O.F. will erect on this 100 foot site stores, offices, lodge halls. For more information, Cochran &amp; McCluer Co. 4622 N Western Ave., 40 N Dearborn St." 
+				</figcaption>
+		</div>
+		<div class="browse-section">
+			<button class="browse-collection-button" onclick="javascript:location.href='http://digital.chipublib.org/digital/collection/rvw/search'">
+				Browse the complete collection
+			</button>
+			<div class="browse-filteredlinks">
+				<p>
+					<h3>Collection highlights</h3>
+					<ul class="highlights">
+						<li class="highlights-li"><a href="http://cdm16818.contentdm.oclc.org/cdm/search/collection/rvw/searchterm/Clubs+%26+organizations/field/genre/mode/all/conn/and/order/nosort">Clubs &amp; Organizations</a></li>
+						<li class="highlights-li"><a href="http://cdm16818.contentdm.oclc.org/cdm/search/collection/rvw/searchterm/Libraries/field/genre/mode/all/conn/and/order/nosort">Libraries</a></li>
+						<li class="highlights-li"><a href="http://cdm16818.contentdm.oclc.org/cdm/search/collection/rvw/searchterm/Parks/field/genre/mode/all/conn/and/order/nosort">Parks</a></li>
+						<li class="highlights-li"><a href="http://cdm16818.contentdm.oclc.org/cdm/search/collection/rvw/searchterm/Religious+institutions/field/genre/mode/all/conn/and/order/nosort">Religious Institutions</a></li>
+						<li class="highlights-li"><a href="http://cdm16818.contentdm.oclc.org/cdm/search/collection/rvw/searchterm/Transportation/field/genre/mode/all/conn/and/order/nosort">Transportation</a></li>
+					</ul>
+					<br />
+				</p>
+			</div>
+				<p>
+					<span class="rights">
+						Should we have a rights statement at the end of each one?
+					</span>
+				</p>
+		</div>
+	</article>
+				<h2 class="main-content-title">
+					Northside Parks Collection
+				</h2>
+		<article class="article-copy">
+			<span>Parks and parks and parks.</span>
+			<div class="article-img">
+				<img src="" />
+				<figcaption>
+					image caption
+				</figcaption>
+			</div>
+			<span>More parks copy.</span>
+			<div class="article-img">
+				<img src="" />
+				<figcaption>
+					Image caption
+				</figcaption>
+			</div>
+			<div class="browse-section">
+				<button class="browse-collection-button" onclick="javascript:location.href='http://digital.chipublib.org/digital/collection/rvw/search'">
+					Browse the complete collection
+				</button>
+				<div class="browse-filteredlinks">
+					<p>
+						<h3>Collection highlights</h3>
+						<ul class="highlights">
+							<li class="highlights-li"><a href="">Some links 1</a></li>
+							<li class="highlights-li"><a href="">Some links 2</a></li>
+							<li class="highlights-li"><a href="">Some links 3</a></li>
+							<li class="highlights-li"><a href="">Some links 4</a></li>
+						</ul>
+						<br />
+					</p>
+				</div>
+				<p>
+					<span class="rights">
+						Should we have a rights statement at the end of each one?
+					</span>
+				</p>
+			</div>
+		</article>
+		<input type="checkbox" id="toggle-3" checked>
+			<label for="toggle-3">
+				<h2 class="main-content-title">
+					Henry Green Collection
+				</h2>
+			</label>
+		</input>
+		<article id="expand-3" class="3-copy">
+			<span>Henry Green was a commercial photographer on Chicago's North Side. </span>
+			<div class="location-card">
+				<img src='http://digital.chipublib.org/digital/api/singleitem/image/hdg/678/default.jpg' onmouseover="this.src='http://digital.chipublib.org/digital/api/singleitem/image/hdg/679/default.jpg';" onmouseout="this.src='http://digital.chipublib.org/digital/api/singleitem/image/hdg/678/default.jpg';" />
+				<!-- <img src="http://digital.chipublib.org/digital/api/singleitem/image/hdg/678/default.jpg" /> -->
+				<figcaption>
+					Street scene of man posing in Mission Beverages company car.  Hover to see reverse side of original photo.
+				</figcaption>
+			</div>
+			<span>He captured on film local businesses, community groups, celebrations in neighborhood parks, events at local schools and life on the North Side during World War II. </span>
+			<div class="location-card">
+				<img src="http://digital.chipublib.org/digital/api/singleitem/image/hdg/341/default.jpg" />
+				<figcaption>
+					First Trolley Bus on Belmont
+				</figcaption>
+			</div>
+		</article>
+		<input type="checkbox" id="toggle-4" checked>
+			<label for="toggle-4">
+				<h2 class="main-content-title">
+					Chicago's Sewers
+				</h2>
+			</label>
+		</input>
+		<article id="expand-4" class="4-copy">
+			<span>The sewer systems and passageways that lie beneath our feet are critical to the health and well being of the city.</span>
+			<div class="location-card">
+				<img src="http://digital.chipublib.org/digital/api/singleitem/image/dtd/167/default.jpg" />
+				<figcaption>
+					103rd Street and Rockwell Street, inspecting tile, above ground, June 1936
+				</figcaption>
+			</div>
+			<span>Offering a rare look at the “underbelly” of Chicago, this collection of historic images features construction photographs of sewers and tunnels.</span>
+			<div class="location-card">
+				<img src="http://digital.chipublib.org/digital/api/singleitem/image/dtd/168/default.jpg" />
+				<figcaption>
+					7 1/2 foot sewer tunnel under 111th Street at heading 1450 west of Eberhart Avenue. Kelly, Foscoe, and Raymond pictured in Pullman Tunnel, April 1938
+				</figcaption>
+			</div>
+			<div class="browse-section">
+				<button class="browse-collection-button" onclick="javascript:location.href='http://digital.chipublib.org/digital/collection/rvw/search'">
+					Browse the complete collection
+				</button>
+				<div class="browse-filteredlinks">
+					<p>
+						<h3>Collection highlights</h3>
+						<ul class="highlights">
+							<li class="highlights-l1"><a href="http://cdm16818.contentdm.oclc.org/cdm/search/collection/dtd/searchterm/Above+ground/field/contri/mode/all/conn/and/order/nosort">Above Ground</a></li>
+							<li class="highlights-l1"><a href="http://cdm16818.contentdm.oclc.org/cdm/search/collection/dtd/searchterm/Avenue+J/field/subjec/mode/all/conn/and/order/nosort">Avenue J</a></li>
+							<li class="highlights-l1"><a href="http://cdm16818.contentdm.oclc.org/cdm/search/collection/dtd/searchterm/California+Avenue/field/subjec/mode/all/conn/and/order/nosort">California Avenue</a></li>
+							<li class="highlights-l1"><a href="http://cdm16818.contentdm.oclc.org/cdm/search/collection/dtd/searchterm/Elston+Avenue/field/subjec/mode/all/conn/and/order/nosort">Elston Avenue</a></li>
+							<li class="highlights-li"><a href="http://chicago.bibliocms.com/fa-chicago-sewers-collection/">Chicago Sewers Collection finding aid</a><br />See the complete list of photographs, glass plate slides and documents, rich with geographic and technical details.</li>
+						</ul>
+						<br />
+					</p>
+				</div>
+					<p>
+						<span class="rights">
+							Should we have a rights statement at the end of each one?
+						</span>
+					</p>
+			</div>
+		</article>
+		
+	</div>
+	<div class="right-sidebar">
+		<div class="blog">
+			<h3>
+				Special Collections Blog
+			</h3>
+			<dl class="blog-list">
+				<dt>Labor of Love: Revs. Addie and Claude Wyatt Photog&hellip; &raquo;</dt>
+				<dd>By: Cynthia August 31, 2017</dd>
+				<dd>Chicagoan Addie Wyatt was a minister, labor activist and&hellip;</dd>
+			</dl>
+			<dl class="blog-list">
+				<dt>Happy 50th to the Chicago Picasso!</dt>
+				<dd>By: Johanna August 4, 2017</dd>
+				<dd>In the early 1960s, the architectural firm Skidmore, Owings &amp; &hellip;</dd>
+			</dl>
+			<dl class="blog-list">
+				<dt>Quiz: How Well Do You Know the Chicago Cubs?</dt>
+				<dd>By: Ellen July 17, 2017</dd>
+				<dd>Can you smell the peanuts and Cracker Jack? It's baseball&hellip;</dd>
+			</dl>
+		</div>
+		<div class="events">
+			<h3>Events</h3>
+			<h4><a href="">Meet the Author: A.C. Barnes <span class="raquo-blue">&raquo;</span></a></h4>
+			<div class="event-location"><a href="">Avalon</a></div>
+			<div class="event-datetime">
+				<div class="event-date">Sep 9</div>
+				<div class="event-time">3:00PM</div>
+			</div>
+			<h4><a href="">Courthouse Discoveries <span class="raquo-blue">&raquo;</span></a></h4>
+			<div class="event-location"><a href="">Sulzer Regional</a></div>
+			<div class="event-datetime">
+				<div class="event-date">Sep 16</div>
+				<div class="event-time">10:00AM</div>
+			</div>
+			<h4><a href="">Latino Punk, History &amp; Music <span class="raquo-blue">&raquo;</span></a></h4>
+			<div class="event-location"><a href="">Lozano</a></div>
+			<div class="event-datetime">
+				<div class="event-date">Sep 16</div>
+				<div class="event-time">1:00PM</div>
+			</div>
+			<h4><a href="">Chicago Transformed: World War I and the Windy City <span class="raquo-blue">&raquo;</span></a></h4>
+			<div class="event-location"><a href="">Clearing</a></div>
+			<div class="event-datetime">
+				<div class="event-date">Sep 18</div>
+				<div class="event-time">6:00PM</div>
+			</div>
+			<h4><a href="">Finding Your Family Tree <span class="raquo-blue">&raquo;</span></a></h4>
+			<div class="event-location"><a href="">Near North</a></div>
+			<div class="event-datetime">
+				<div class="event-date">Sep 18</div>
+				<div class="event-time">6:30PM</div>
+			</div>
+			<span><a href="">View More &raquo;</a></span>
+		</div>
+	</div>
+</div>
+	<script>
+		function showDropdown() {
+			document.getElementById("show-dropdown").classList.toggle("show");
+		}
+		window.onclick = function(e) {
+			if (!e.target.matches('.header-browse-button')) {
+				var myDropdown = document.getElementById("show-dropdown");
+				if (myDropdown.classList.contains('show')) {
+						myDropdown.classList.remove('show');
+				}
+			}
+		}
+		function searchExpander() {
+            if (document.getElementById('search-collapsed')) {
+
+                if (document.getElementById('search-collapsed').style.display == 'none') {
+	                    document.getElementById('search-collapsed').style.display = 'block';
+	                    document.getElementById('search-expanded').style.display = 'none';
+                }
+                else {
+                    document.getElementById('search-collapsed').style.display = 'none';
+                    document.getElementById('search-expanded').style.display = 'block';
+                }
+            }
+		}
+	</script>
+</body>
+</html>
