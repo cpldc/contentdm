23163f37e56f91b03f93c3690f9062303d8a158f
dad <dad@desk.cable.rcn.com>
2017-09-21 22:48:51 -0500

    implements African Americans with bootstrap

--- php/AfricanAmericans.php
+++ php/AfricanAmericans.php
@@ -2,98 +2,135 @@
 <html lang="en">
 <head>
 <?php 
-	$PAGE_TITLE = 'African Americans | ';
+	$PAGE_TITLE = 'African Americans';
 ?>
 	<meta charset="UTF-8">
-	<title> <?php echo $PAGE_TITLE ?>Digital Collections | Chicago Public Library</title>
+	<title> <?php echo $PAGE_TITLE ?> Digital Collections | Chicago Public Library</title>
 	    <link rel="shortcut icon" type="image/x-icon" href="https://chicago.bibliocms.com/wp-content/uploads/sites/3/2016/04/CPLOpenBook_web_16x16-1.png">
 		<link rel="apple-touch-icon-precomposed" type="image/x-icon" href="https://chicago.bibliocms.com/wp-content/uploads/sites/3/2016/04/CPLreverse_web_200x200-150x150.png">
-		<link rel="stylesheet" href="cpldc.css">
+
 		<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
-		<script type="text/javascript" src="cpldc.js" ></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<style>
+	div {
+		background-color: rgba(0,0,0,0.1);
+		border: 1px solid #fff;
+	}
+	.header-logo-img img, .header-sm-img img {
+		max-height: 65px;
+	}
+	.header-search-collapsed {
+	}
+	.header-search-collapsed div, .header-lg-button, .header-sm-button, .header-sm-img, .footer-list {
+		display: inline-block;
+	}
+	.footer-list ul {
+		list-style-type: none;
+	}
+</style>
 </head>
 <body>
-<?php 
-	include 'header.html';
-?>
-<div class="body-full-f">
-	<?php
-		include 'sidebar-nav-fixed.html';
-	?>
-	<div class="main-content-f">
-		<input type="checkbox" id="toggle-mrc" checked>
-			<label for="toggle-mrc">
-				<h2 class="main-content-f-title">
-					African Americans
-				</h2>
-			</label>
-		</input>
-		<article id="expand-mrc" class="mrc-copy">
-			<span>Summary of Category, one to two lines.</span>
-			<div class="location-card">
-				<img src="http://digital.chipublib.org/digital/api/singleitem/image/mpu/6324/default.jpg" />
-				<figcaption>
-					Image Caption
-				</figcaption>
+<header>
+	<div class="container">
+		<div class="row justify-content-between align-items-center">
+		<div class="hidden-sm-down col-md-3">
+			<div class="header-logo-img"><img src="https://cor-liv-cdn-static.bibliocommons.com/images/IL-CPL/logo.png?1506021413180" alt="" /></div>
+			<div class="header-logo-text"><span>Digital Collections</span></div>
+		</div>
+		<div class="hidden-md-up col-4">
+			<div class="header-sm-img">
+				<img src="https://cor-liv-cdn-static.bibliocommons.com/images/IL-CPL/mobile_logo.png?1505979997263" alt="" />
 			</div>
-			<div class="image-gallery-thumbs"><img src="http://digital.chipublib.org/digital/api/singleitem/image/cfc/79/default.jpg" alt="Board of Directors of the Ravenswood Manor Improvement Association rehearsing for the Winter Social"></div>
-			<div class="image-gallery-thumbs"><img src="http://digital.chipublib.org/digital/api/singleitem/image/cfc/79/default.jpg" alt="Board of Directors of the Ravenswood Manor Improvement Association rehearsing for the Winter Social"></div>
-			<div class="image-gallery-thumbs"><img src="http://digital.chipublib.org/digital/api/singleitem/image/cfc/79/default.jpg" alt="Board of Directors of the Ravenswood Manor Improvement Association rehearsing for the Winter Social"></div>
-			<div class="image-gallery-thumbs"><img src="http://digital.chipublib.org/digital/api/singleitem/image/cfc/79/default.jpg" alt="Board of Directors of the Ravenswood Manor Improvement Association rehearsing for the Winter Social"></div>
-<div class="main-richcopy">
-	<div><b>Rich Copy, links and descriptions - length?</b>  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed esse ratione ut quo doloribus perferendis veritatis facere similique cupiditate voluptatem repudiandae consequuntur, sit quidem officiis sunt nobis magnam nesciunt numquam. Quia enim repudiandae labore sequi id. Hic alias explicabo quia nulla <b>itaque maxime dolorem aspernatur</b> fugit odio harum facilis veniam exercitationem veritatis libero voluptates rem minima laborum delectus, perferendis enim. Corporis modi culpa asperiores consectetur, dolorem sint, commodi maxime, qui fugiat sapiente rem? Molestias iure consequatur nostrum ab magni maiores suscipit, repudiandae earum voluptates. Voluptates asperiores delectus quidem, neque voluptatem.</div>
-	<div>Assumenda maiores dolores sequi vitae, repudiandae fugit asperiores excepturi corrupti placeat nostrum. Facere at possimus alias laboriosam explicabo minus <b>nisi</b> impedit <b>assumenda</b>. Nisi unde, illo animi odio voluptatum perspiciatis nulla. Quasi minus, neque nihil vitae repellendus <b>accusantium est perferendis assumenda, eveniet</b> totam voluptatibus provident maxime corporis consequatur doloremque cumque sit pariatur suscipit dignissimos sapiente libero harum optio iure? Tempora, velit! <ul><li><b>Consequatur corporis dolorem soluta</b></li> <li><b>enim facere odit mollitia</b></li> <li><b>temporibus recusandae sapiente</b></li><li><b>Officiis aut ab non</b></li> <li><b>minima aperiam nam delectus</b></li></ul>Natus rem blanditiis incidunt nesciunt asperiores laboriosam, similique nobis sed animi.</div>
-	<div>Ratione cumque quam, eos necessitatibus, quis reprehenderit iure quo quas qui distinctio rem, in eligendi deserunt voluptatem! Sit maxime odit eveniet aspernatur sunt assumenda reprehenderit. Aut ullam ex vitae? Soluta?</div>
-	<div>Tempora a fugit doloribus accusantium, harum reprehenderit, deserunt. Repellat, id eligendi tempora nesciunt illum sunt labore laudantium accusamus consectetur dignissimos, sint architecto <b>voluptatem</b> neque <b>dolorum</b> dolor <b>quaerat</b>. Voluptatum, eius, quae! Debitis corrupti consequuntur voluptas inventore, sequi illum voluptatibus quidem alias quae corporis, fuga delectus eius dolorum sapiente quod quam commodi voluptates magni impedit? Deserunt ipsa quaerat doloremque consectetur odio ex! Repellendus ipsa illum exercitationem optio magnam consequatur, officiis temporibus tenetur laudantium quisquam rem nihil obcaecati ut eveniet, sunt at iste labore minima totam corporis debitis placeat inventore. Animi ut, obcaecati.</div>
-</div>
-			<div class="browse-section">
-				<button class="browse-collection-button" onclick="javascript:location.href='https://chipublib.bibliocommons.com/events/search/index'">
-					Browse the complete collection
-				</button>
-				<div class="browse-filteredlinks">
-					<p>
-						<a href="http://cdm16818.contentdm.oclc.org/cdm/search/collection/mpu/searchterm/highlight/field/highli/mode/all/conn/and/order/origin">
-							Collection highlights
-						</a>
-						<br />
-						<span class="browse-link-caption">
-							Get a taste of how this transformation took place in a selected group of images.
-						</span>
-					</p>
-					<p>
-						<a href="http://chicago.bibliocms.com/fa-millennium-park-u-s-equities-realty-collection/">
-							Millennium Park / U.S. Equities Realty Collection finding aid
-						</a>
-						<br />
-						<span class="browse-link-caption">
-							Learn about the construction of the park and the Chicago-based company that made it possible.
-						</span>
-					</p>
+			<div class="header-sm-button"><i class="fa fa-bars"></i></div>
+			<div class="header-sm-button"><i class="fa fa-calendar"></i></div>
+		</div>
+		<div class="col-3 col-md-2">
+			<div class="header-search-collapsed">
+				<div class="header-search-collapsed-text">Search</div>
+				<div class="header-search-collapsed-icon"><i class="fa fa-search"></i></div>
+			</div>
+		</div>
+		</div>
+		<div class="row">
+			<div class="hidden-sm-down col-md-4 offset-md-1">
+				<div class="header-lg-button">Browse<i class="fa fa-angle-down"></i></div>
+				<div class="header-lg-button"><i class="fa fa-calendar"></i>Events</div>
+			</div>
+		</div>
+	</div>
+</header>
+<main>
+	<div class="container">
+		<div class="row">
+			<div class="col-4 col-lg-3">
+				left sidebar
+			</div>
+			<div class="col-8 col-lg-6">
+				<h1><?php echo $PAGE_TITLE; ?></h1>
+				main content
+			</div>
+			<div class="hidden-md-down col-lg-3">
+				right sidebar
+			</div>
+		</div>
+	</div>
+</main>
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-4">
+				<div class="footer-list">
+					<ul>
+						<li><a href="https://www.facebook.com/pages/Chicago-Public-Library/35447572453"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="https://twitter.com/chipublib"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="https://www.instagram.com/chicagopubliclibrary/"><i class="fa fa-instagram"></i></a></li>
+						<li><a href="https://visitor.r20.constantcontact.com/d.jsp?llr=vs7doizab&p=oi&m=1126638416469&sit=fcnca6dlb&f=a1f24030-412d-44ba-8dc8-d4bbd69edbd4"><i class="fa fa-newspaper-o"></i></a></li>
+					</ul>
+				</div>
+				<div class="footer-list">
+					<ul>
+						<li><a href=""></a>Home</li>
+						<li><a href=""></a>Browse</li>
+						<li><a href=""></a>About</li>
+						<li><a href=""></a>Contact?</li>
+					</ul>
 				</div>
 			</div>
-					<p>
-						<span class="rights">
-							The Millennium Park Digital Collection was made possible by generous gifts from U.S. Equities Realty and the Gaylord and Dorothy Donnelley Foundation through the Chicago Public Library Foundation.
-						</span>
-					</p>
-		</article>
-	</div>
-	<div class="right-sidebar">
-		<div class="blogs">
-			<?php
-				include 'blogs.php';
-			?>
-
+			<div class="col-4">
+				<div class="footer-list">
+					<ul>
+						<li><a href=""></a>Home</li>
+						<li><a href=""></a>Browse</li>
+						<li><a href=""></a>About</li>
+						<li><a href=""></a>Contact?</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="footer-contactinfo">
+					<dl>
+						<dt>Contact Us</dt>
+						<dd>Chicago Public Library</dd>
+						<dd>400 S. State Street</dd>
+						<dd>Chicago, IL 60605</dd>
+						<dd>(312) 747-4300</dd>
+					</dl>
+				</div>
+			</div>
 		</div>
-		<div class="events">
-			<?php 
-				include 'events.php';
-			?>
+		<div class="row justify-content-between align-items-center">
+			<div class="col-3 col-md-3">
+				<div class="footer-logo-img"><img src="https://chicago.bibliocms.com/wp-content/uploads/sites/3/2016/02/CPLSupportCPLF_web_130x60.png?v=1505779744150575491515057548853" alt="" /></div>
+			</div>
+			<div class="col-4 col-md-4">
+				<div class="footer-bottom-right">
+					<a href="link">City of Chicago</a>&nbsp;&copy; 2017 Chicago Public Library
+				</div>
+			</div>
 		</div>
 	</div>
-</div>
-<?php 
-	include 'footer.html';
-?>
+</footer>
 </body>
 </html>
