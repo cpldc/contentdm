b57134b96b9f2f19217b825f573a867eda674e47
dad <dad@desk.cable.rcn.com>
2017-09-22 22:24:12 -0500

    more more more...AfAm...

--- php/AfricanAmericans.php
+++ php/AfricanAmericans.php
@@ -10,6 +10,7 @@
 		<link rel="apple-touch-icon-precomposed" type="image/x-icon" href="https://chicago.bibliocms.com/wp-content/uploads/sites/3/2016/04/CPLreverse_web_200x200-150x150.png">
 		<link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/css/tether-theme-arrows-dark.css" integrity="sha256-h23kMHZF9EZB4pGSPypdDjFgPawq5Hmdr2X0jD7MCVE=" crossorigin="anonymous" />
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 		<link rel="stylesheet" href="AfAm.css" >
@@ -26,53 +27,133 @@
 <header>
 	<div class="container">
 		<div class="row justify-content-between align-items-center">
-			<div class="hidden-sm-down col-md-3">
+			<div class="hidden-md-down col-lg-3">
 				<div class="header-logo-img"><a href="https://www.chipublib.org/"><img src="https://cor-liv-cdn-static.bibliocommons.com/images/IL-CPL/logo.png?1506021413180" alt="" /></a></div>
 				<div class="header-logo-text"><a href="">Digital Collections</a></div>
 			</div>
-			<div class="hidden-md-up col-4">
+			<div class="hidden-lg-up col-6">
 				<div class="header-sm-img">
 					<a href="https://www.chipublib.org/">
 						<img src="https://cor-liv-cdn-static.bibliocommons.com/images/IL-CPL/mobile_logo.png?1505979997263" alt="" />
 					</a>
 				</div>
-				<div class="header-sm-button"><i class="fa fa-bars"></i></div>
-				<div class="header-sm-button"><i class="fa fa-calendar"></i></div>
+				<div class="header-sm-button" data-toggle="modal" data-target="#myModal"><i class="fa fa-bars"></i></div>
+				<div class="header-sm-button light-left-border" onclick="javascript:location.href='https://chipublib.bibliocommons.com/events/search/index'"><i class="fa fa-calendar"></i></div>
 			</div>
-			<div class="col-8 clearfix">
+			<div class="col-3 col-lg-8 clearfix">
 				<div class="header-search-collapsed" onclick="expandSearch()">
 					<div class="header-search-collapsed-text hidden-sm-down">Search</div>
-					<div class="header-search-collapsed-icon"><i class="fa fa-search"></i></div>
+					<div class="header-search-collapsed-icon" ><i class="fa fa-search"></i></div>
 				</div>
 				<div class="header-search-expanded hide">
-					<div class="header-search-expanded-text hidden-md-down" onclick="SEARCHS!!!">Search the Digital Collections</div>
-					<div class="header-search-expanded-text hidden-lg-up" onclick="SEARCHS!!!">Search</div>
+					<div class="header-search-expanded-text hidden-lg-down" onclick="SEARCHS!!!">Search the Digital Collections</div>
+					<div class="header-search-expanded-text hidden-xl-up" onclick="SEARCHS!!!">Search</div>
 					<div class="header-search-expanded-input"><input type="text"></div>
 					<div class="header-search-expanded-icon"><i class="fa fa-search"></i></div>
 				</div>
 			</div>
 		</div>
-		<div class="row">
-			<div class="hidden-sm-down col-md-4 offset-md-1">
+		<div class="row align-items-center">
+			<div class="hidden-md-down col-lg-12">
 				<div class="header-bottom">
-					<div class="header-lg-button">Browse<i class="fa fa-angle-down"></i></div>
-					<div class="header-lg-button"><i class="fa fa-calendar"></i>Events</div>
+					<div class="header-lg-button" onclick="showDropdown()">Browse <i class="fa fa-angle-down"></i></div>
+					<div class="header-lg-button" onclick="javascript:location.href='https://chipublib.bibliocommons.com/events/search/index'"><i class="fa fa-calendar"></i> Events</div>
 				</div>
 			</div>
 		</div>
 
 	</div>
-	<div class="header-search-dropped clearfix">
+</header>
+<div class="container header-search-dropped hide clearfix" onclick="hideSearch()">
+	<div class="header-search-dropped-close"><i class="fa fa-times fa-2x"></i></div>
+	<div class="row header-search-dropped-search no-gutters align-items-center clearfix">
+		<div class="col-2 header-search-dropped-text">
+			Search
+		</div>
+		<div class="col-8 header-search-dropped-input">
+			<input type="text"></input>
+		</div>
+		<div class="col-1 header-search-dropped-icon">
+			<i class="fa fa-search"></i>
+		</div>
+		<br />
+		<span class="header-search-dropped-adv"><a href="">Advanced Search</a></span>
+	</div>
+</div>
+<section class="header-browse-dropdown-wrapper hide" onclick="hideDropdown()">
+	<div class="container header-browse-dropdown">
 		<div class="row">
-			<div class="header-search-dropped-text clearfix">Search</div>
-			<div class="header-search-dropped-search clearfix">
-				<div class="header-search-dropped-input"><input type="text"></div>
-				<div class="header-search-dropped-icon"><i class="fa fa-search"></i></div>
+			<div class="col-lg-12 hidden-md-down">
+				<div class="header-browse-dropdown-browse"><h2>Browse</h2></div>
+				<div class="header-browse-dropdown-section header-browse-dropdown-format">
+					<span class="header-browse-dropdown-title">By Format</span>
+					<ul class="list-group">
+						<li class="list-group-item header-dropdown-listitem"><a href="http://www.chipublib.org/browse/books/">Books</a></li>
+						<li class="list-group-item header-dropdown-listitem"><a href="http://www.chipublib.org/browse/ebooks/">eBooks</a></li>
+						<li class="list-group-item header-dropdown-listitem"><a href="http://www.chipublib.org/browse/audiobooks/">Audiobooks</a></li>
+						<li class="list-group-item header-dropdown-listitem"><a href="http://www.chipublib.org/browse/movies/">Movies &amp; TV</a></li>
+						<li class="list-group-item header-dropdown-listitem"><a href="http://www.chipublib.org/browse/music/">Music</a></li>
+					</ul>
+				</div>
+				<div class="header-browse-dropdown-section header-browse-dropdown-aud">
+					<span class="header-browse-dropdown-title">By Audience</span>
+					<ul class="list-group">
+						<li class="list-group-item header-dropdown-listitem"><a href="http://www.chipublib.org/browse/kids/">Kids</a></li>
+						<li class="list-group-item header-dropdown-listitem"><a href="http://www.chipublib.org/browse/teens/">Teens</a></li>
+					</ul>
+				</div>
+				<div class="header-browse-dropdown-section header-browse-dropdown-more">
+					<span class="header-browse-dropdown-title">More</span>
+					<ul class="list-group">
+						<li class="list-group-item header-dropdown-listitem"><a href="http://www.chipublib.org/online-resources/">Online Resources</a></li>
+						<li class="list-group-item header-dropdown-listitem"><a href="http://www.chipublib.org/browse/jobs-small-business/">Jobs &amp; Small Business</a></li>
+						<li class="list-group-item header-dropdown-listitem"><a href="http://www.chipublib.org/browse/chicago-history/">Chicago History</a></li>
+						<li class="list-group-item header-dropdown-listitem"><a href="http://www.chipublib.org/archives-collections/">Archival Collections</a></li>
+						<li class="list-group-item header-dropdown-listitem"><a href="https://www.chipublib.org/browse_program/one-book-one-chicago/">One Book, One Chicago</a></li>
+					</ul>
+				</div>
 			</div>
-			<div class="header-search-dropped-adv"><a href="">Advanced Search</a></div>
 		</div>
 	</div>
-</header>
+</section>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog" role="document">
+  <div class="modal-content">
+	<div class="modal-header">
+	  <h5 class="modal-title" id="exampleModalLabel">Browse <i class="fa fa-angle-down"></i></h5>
+	  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		<span aria-hidden="true">&times;</span>
+	  </button>
+	</div>
+	<div class="modal-body">
+			<h4 class="header-modal-listheader">By Format</h4>
+		<ul class="list-group">
+			<li class="list-group-item header-modal-listitem"><a href="http://www.chipublib.org/browse/books/">Books</a></li>
+			<li class="list-group-item header-modal-listitem"><a href="http://www.chipublib.org/browse/ebooks/">eBooks</a></li>
+			<li class="list-group-item header-modal-listitem"><a href="http://www.chipublib.org/browse/audiobooks/">Audiobooks</a></li>
+			<li class="list-group-item header-modal-listitem"><a href="http://www.chipublib.org/browse/movies/">Movies &amp; TV</a></li>
+			<li class="list-group-item header-modal-listitem"><a href="http://www.chipublib.org/browse/music/">Music</a></li>
+		</ul>
+		<h4 class="header-modal-listheader">By Audience</h4>
+		<ul class="list-group">
+			<li class="list-group-item header-modal-listitem"><a href="http://www.chipublib.org/browse/kids/">Kids</a></li>
+			<li class="list-group-item header-modal-listitem"><a href="http://www.chipublib.org/browse/teens/">Teens</a></li>
+		</ul>
+		<h4 class="header-modal-listheader">More</h4>
+		<ul class="list-group">
+			<li class="list-group-item header-modal-listitem"><a href="http://www.chipublib.org/online-resources/">Online Resources</a></li>
+			<li class="list-group-item header-modal-listitem"><a href="http://www.chipublib.org/browse/jobs-small-business/">Jobs &amp; Small Business</a></li>
+			<li class="list-group-item header-modal-listitem"><a href="http://www.chipublib.org/browse/chicago-history/">Chicago History</a></li>
+			<li class="list-group-item header-modal-listitem"><a href="http://www.chipublib.org/archives-collections/">Archival Collections</a></li>
+		</ul>
+	</div>
+	<div class="modal-footer">
+	  <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	</div>
+  </div>
+</div>
+</div>
+
 <main>
 	<div class="container">
 		<div class="row">
@@ -185,7 +266,7 @@
 						Chicago Public Library<br>
 						400 S. State Street<br>
 						Chicago, IL 60605<br>
-						<abbr title="Phone">P:</abbr> (312) 747-4300
+						(312) 747-4300
 					</address>
 				</div>
 			</div>
@@ -204,18 +285,41 @@
 </footer>
 
 
+
 <script>
 	function expandSearch() {
+		var viewportWidth = $(window).width();
 		$(".header-search-collapsed").addClass("hide");
-		$(".header-search-expanded").removeClass("hide");
+		if (viewportWidth > 976) {
+			$(".header-search-expanded").removeClass("hide");
+		}
+		if (viewportWidth < 976) {
+			$(".header-search-dropped").removeClass("hide");
+		}
+	}
+	function hideSearch() {
+		$(".header-search-dropped").addClass("hide");
+		$(".header-search-collapsed").removeClass("hide");
+	}
+	function showDropdown() {
+		$(".header-browse-dropdown-wrapper").removeClass("hide");
+	}
+	function hideDropdown() {
+		$(".header-browse-dropdown-wrapper").addClass("hide");
 	}
 	$(window).resize(function () {
 		var viewportWidth = $(window).width();
-		if (viewportWidth < 768) {
-			// $(".sidebar-hide-button").removeClass("hide").addClass("show");
+		if (viewportWidth < 976) {
+			if (!($(".header-search-expanded").hasClass("hide"))) {
+				$(".header-search-expanded").addClass("hide");
+				$(".header-search-dropped").removeClass("hide");
+			}
 		}
-		if (viewportWidth > 768) {
-			// $(".sidebar-hide-button").removeClass("show").addClass("hide");
+		if (viewportWidth > 976) {
+			if (!($(".header-search-dropped").hasClass("hide"))) {
+				$(".header-search-dropped").addClass("hide");
+				$(".header-search-expanded").removeClass("hide");
+			}
 		}
 	});
 </script>
