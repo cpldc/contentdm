ead3a68219b6f0d3b729dbecd06ae7e305f5080b
Nick White <nwhite@chipublib.org>
2018-02-07 09:51:20 -0600

    cleans out old files

--- json-encode.php
+++
@@ -1,5 +0,0 @@
-<?php
-include 'variables.php';
-$json = json_encode ( $hdg );
-file_put_contents('json.json', $json);
-?>
--- prereduction/cardsecho.php
+++
@@ -1,6 +0,0 @@
-<pre>
-<?php 
-include 'variables.php';
-print_r($cards);
-?>
-</pre>
