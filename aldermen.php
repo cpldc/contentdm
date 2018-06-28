48eb75b7225ee051a69228c23b00560e19bb73f1
cpldc <cpldc@localhost.localdomain>
2018-06-28 12:15:30 -0500

    adds CAA content; adds functional aldermen content

--- aldermen.php
+++ aldermen.php
@@ -20,6 +20,7 @@
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<script src="aldermen.js"></script>
+	<script src="aldermen-json.js"></script>
 	<link rel="stylesheet" href="cpldc.css" >
 	<link rel="stylesheet" href="aldermen.css" >
 
